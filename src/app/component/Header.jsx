8b3fb4657294e51bace2fcf2f3712f465f4524ff
Iqra Fatima <iqrafatima544@gmail.com>
2025-07-10 18:00:03 +0500

    first commit

---
+++ src/app/component/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header(){
+    return(
+        <>
+        <h1>Header</h1>
+        </>
+    )
+}
+
+
+export function iqra()
+{
+    <>
+    <h1>Iqra Fatima</h1>
+    </>
+}
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Header,{iqra} from "./component/Header";
+
+export default function Home() {
+  return (
+ <>
+ <Header></Header>
+ 
+ </>
+  );
+}
